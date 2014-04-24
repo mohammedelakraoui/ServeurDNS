4be06f38ed0251f8f1c49b170847263f2bdd075c
PIERRE Jean-Ernsso <ernsso@hotmail.fr>
2014-04-24 11:49:13 +0200

    header finish

---
+++ DNSClient/src/fr/esgi/project/dns/DnsClient.java
@@ -0,0 +1,16 @@
+package fr.esgi.project.dns;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.nio.ByteBuffer;
+import java.nio.ShortBuffer;
+import java.util.Random;
+
+/**
+ * Created by server-pc on 04/04/14.
+ */
+public class DnsClient {
+
+}
---
+++ DNSClient/src/fr/esgi/project/dns/DnsQuery.java
@@ -0,0 +1,14 @@
+package fr.esgi.project.dns;
+import java.nio.ByteBuffer;
+import java.util.Random;
+
+//	labels          63 octets or less
+//	names           255 octets or less
+//	TTL             positive values of a signed 32 bit number.
+//	UDP messages    512 octets or less
+
+public class DnsQuery {
+	public DnsQuery() {
+		
+	}
+}
